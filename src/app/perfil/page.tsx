cf0672d6b24d2d14d43d215634a8b3b34cdb7c18
PEDRO DAVID <97pedro@live.com.mx>
2024-11-18 10:05:11 -0600

    feat: account

--- src/app/perfil/page.tsx
+++ src/app/perfil/page.tsx
@@ -1,6 +1,8 @@
 import { AccountInfoCard } from "@/components/account/AccountInfoCard";
 import { SignOutButton } from "@/components/auth/SignOutButton";
 import styles from "@/styles/pages/account/Account.module.css";
+import { ArrowRight01Icon } from "hugeicons-react";
+import Link from "next/link";
 
 export default function Account() {
   return (
@@ -10,7 +12,12 @@ export default function Account() {
         <AccountInfoCard />
       </section>
       <div className={styles.sections_container}>
-        <section className={styles.section}></section>
+        <section className={styles.section}>
+          <Link href="/perfil/editar" className={styles.link}>
+            Editar perfil
+            <ArrowRight01Icon size={24} />
+          </Link>
+        </section>
         <section className={styles.section}>
           <SignOutButton />
         </section>
