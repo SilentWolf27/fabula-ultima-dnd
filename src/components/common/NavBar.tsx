e6548e3625af0350dc2276f5677c98cf34ce6b55
Pedro <pedro.juarez@uaslp.mx>
2024-11-24 07:11:40 -0600

    enhance navbar route handling

--- src/components/common/NavBar.tsx
+++ src/components/common/NavBar.tsx
@@ -9,6 +9,14 @@ export const NavBar = () => {
   const currentPath = usePathname();
   const { items } = useNavbar();
 
+  const getLinkIsActive = (path: string) => {
+    if (currentPath.includes(path) && path !== "/dashboard") {
+      return true;
+    }
+
+    return currentPath === path;
+  };
+
   return (
     <nav className={styles.navbar}>
       <ul className={styles.navbar__list}>
@@ -16,7 +24,7 @@ export const NavBar = () => {
           <li
             key={index}
             className={`${styles.navbar__item} ${
-              currentPath === item.path ? styles.active : ""
+              getLinkIsActive(item.path) ? styles.active : ""
             }`}>
             <Link href={item.path} className={styles.navbar__link}>
               {item.icon ? <item.icon /> : null}
