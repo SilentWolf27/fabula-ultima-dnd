a7a877e8f4219f9629a69cf831474c91ee3511e5
Pedro <pedro.juarez@uaslp.mx>
2024-12-01 22:37:01 -0600

    migration to tailwind v1

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,74 +1,98 @@
 "use client";
 
-import { loginAction, LoginState } from "@/actions/auth/auth";
-import { useActionState, useState } from "react";
+import { loginAction } from "@/actions/auth/auth";
 import { ViewIcon, ViewOffSlashIcon } from "hugeicons-react";
-import styles from "@/styles/components/auth/LoginForm.module.css";
-
-const initialFormState = {
-  message: null,
-  email: "",
-  password: "",
-};
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { loginSchema } from "@/schemas/auth/auth";
+import { z } from "zod";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  Form,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { useState } from "react";
 
 export const LoginForm = () => {
-  const [showPassword, setShowPassword] = useState(false);
+  const [showPassword, setShowPassword] = useState<boolean>(false);
 
-  const [formState, formAction, isFormPending] = useActionState<
+  /* const [formState, formAction, isFormPending] = useActionState<
     LoginState,
     FormData
-  >(loginAction, initialFormState);
+  >(loginAction, initialFormState); */
+
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (data: z.infer<typeof loginSchema>) => {
+    console.log(data);
+  };
 
   return (
-    <form action={formAction} className={styles.form}>
-      <div className={styles.input_container}>
-        <label htmlFor="email" className={styles.label}>
-          Email
-        </label>
-        <input
-          type="text"
-          id="email"
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="mt-8 flex flex-col gap-5">
+        <FormField
+          control={form.control}
           name="email"
-          className={styles.input}
-          placeholder="username@fabula.com"
-          defaultValue={formState.email}
-          autoComplete="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Correo electrónico</FormLabel>
+              <FormControl>
+                <Input placeholder="Ej. username@fabula.com" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
         />
-      </div>
 
-      <div className={styles.input_container}>
-        <label htmlFor="password" className={styles.label}>
-          Contraseña
-        </label>
-        <div className={styles.password_container}>
-          <input
-            type={showPassword ? "text" : "password"}
-            id="password"
-            name="password"
-            className={styles.input}
-            placeholder={showPassword ? "Contraseña" : "*********"}
-            defaultValue={formState.password}
-            autoComplete="current-password"
-          />
-          <button
-            type="button"
-            className={styles.eye_button}
-            onClick={() => setShowPassword(!showPassword)}>
-            {showPassword ? <ViewOffSlashIcon /> : <ViewIcon />}
-          </button>
-        </div>
-      </div>
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Contraseña</FormLabel>
+              <FormControl>
+                <div className="relative">
+                  <Input
+                    placeholder="********"
+                    {...field}
+                    type={showPassword ? "text" : "password"}
+                    className="w-full outline-violet-700"
+                  />
+                  <button
+                    type="button"
+                    onClick={() => setShowPassword(!showPassword)}
+                    className="absolute right-2 top-2 text-violet-700 outline-none border-none">
+                    {showPassword ? (
+                      <ViewOffSlashIcon size={24} />
+                    ) : (
+                      <ViewIcon size={24} />
+                    )}
+                  </button>
+                </div>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
-      <button
-        type="submit"
-        className={styles.submit_button}
-        disabled={isFormPending}>
-        {isFormPending ? "Iniciando sesión..." : "Iniciar sesión"}
-      </button>
-
-      {formState.message && (
-        <p className={styles.error_message}>{formState.message}</p>
-      )}
-    </form>
+        <Button className="bg-violet-700 text-base" type="submit">
+          {" "}
+          Iniciar sesión{" "}
+        </Button>
+      </form>
+    </Form>
   );
 };
